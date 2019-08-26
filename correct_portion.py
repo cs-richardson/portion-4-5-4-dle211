a594e48d16c320c6f7ff41f875b7b80c1a1d6574
Dung (Justin) Le <dle21@ssis.edu.vn>
2019-08-26 11:02:24 +0700

    Update correct_portion.py

--- correct_portion.py
+++ correct_portion.py
@@ -4,11 +4,11 @@ num_people = 25
 
 # Determine how much food each person gets
 tons_of_food_per_person = tons_of_food / num_people
-print tons_of_food_per_person
+print(tons_of_food_per_person)
 
 # Ask the user how much food they took
 tons_taken = float(input("How many tons of food did you take? "))
 if round(tons_taken, 5) == round(tons_of_food_per_person, 5):
-    print "Good job, you took the right amount of food!"
+    print("Good job, you took the right amount of food!")
 else:
-    print "You took the wrong amount of food!"
+    print("You took the wrong amount of food!")
